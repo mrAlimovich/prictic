3b19e71518d3cbfb3b5ad5aef62e551c34f69bf6
sammet <dg.ramz177@gmail.com>
2026-05-24 21:07:53 +0500

    feat: learn primitive types in python

---
+++ primitive.py
@@ -0,0 +1,17 @@
+print("===== number =====")
+# in JAVA, variable is a name storage location!
+# in Python, variable is named reference!
+
+count = 100
+count_type = type(count)
+print(f"the count: {{count and type: {count_type}}}")
+
+return1 = count.bit_count() = method
+return2 = count.numerator  # state
+print(result1, result2)
+
+print("===== string =====")
+
+course = "AI Python FullStack"
+result3 = type(course)
+print("the type of course: {result3}")
