0e97149f3f56a0e20d244b57b78b6a66fbf7895c
sammet <dg.ramz177@gmail.com>
2026-05-25 21:45:19 +0500

    feat: study iterable objects

---
+++ Iterable.py
@@ -0,0 +1,29 @@
+print("==== Iterable object & RANGE ====")
+# Iterable object > string dict tuple list range map filter
+
+range_obj = range(3)
+print("range_obj:", range_obj)
+
+text = "MIT"
+for letter in text:
+    print(f"the letter: {letter}")
+for ele in range_obj:
+    print(f"the element: {ele}")
+
+print("==== DICTIONARY ====")
+# DICTIONARY is JSON object!
+person = {"name": "Odil", "age": 25, "single": True}
+person_obj = dict(name="Odil", age=25, single=True)
+print(f"the person: {person}")
+print(f"the person_obj: {person_obj}")
+
+# method: get()
+# name = person_obj("name")
+name = person_obj.get("name")
+hobby = person_obj.get("hobby")
+balance = person_obj.get("balance", 0)
+print(f"the name: {name}, hobby: {hobby} and balance: {balance}")
+
+del person_obj["single"]
+for key in person_obj:
+    print(f"the key: {key} => value {person_obj.get(key)}")
