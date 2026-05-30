4ac974f535c4d54c439af3fa8c30b4c7e090b26d
sammet <dg.ramz177@gmail.com>
2026-05-30 15:46:17 +0500

    feat: unpacking argument via tuple

--- tuples.py
+++ tuples.py
@@ -19,13 +19,41 @@ print("before fruits:", fruits)
 fruits[2] = "melon"
 print("after fruits:", fruits)
 
-# Tuple
+# we can not mutate Tuple
 animals = ("dog", "cat", "fish", "Lion")
 tuple_pbj = ("MIT", 100, True, None)
 
 print(animals[0])
-animals[0] = "bird"
+# animals[0] = "bird"
+
+# try avoid this
+peaople = "Odil", "Ika"
+animals = "dog",
 
 
 print("==== Unpacking arguments ====")
+groups = ["MIT", "FLEXY", "DEVEX", "MG"]
+(x, y, *z) = groups
+print(f"the x: {x} and y: {y}")
+print("z:", z)  # list
+
+
+# *args > tuple
+def calculate(*args):
+    print("*args", args)
+    total = 1
+    for x in args:
+        total *= x
+    print(f"the total value: {total}")
+    return total
+
+
+# CALL
+calculate(1, 7, 2, 3)
+print("-----")
+calculate(0, 2, 300)
+print("-----")
+calculate(5, 7)
+
+
 print("==== Zip ====")
