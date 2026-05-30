bda94786318b11b5e0e6707948ec47ea28463ee0
sammet <dg.ramz177@gmail.com>
2026-05-30 16:10:40 +0500

    feat: learn zip object

--- tuples.py
+++ tuples.py
@@ -78,3 +78,10 @@ greating("HI", True, 10, name="Odil", age=22)
 
 
 print("==== Zip ====")
+tuple1 = (1, 2, 3, 4)
+tuple2 = ('a', 'b', 'c')
+
+zipped = zip(tuple1, tuple2)
+print("zipped:", zipped)
+result = list(zipped)
+print(f"the result: {result}")
